e96d06c073b100c17f33e9e88d51d1db59f44cbd
Francesc Campoy <campoy@google.com>
2016-11-06 16:14:21 -0800

    add embedding a file in different directory on the samples
    Change-Id: I32cda1446a98b82fdbd83816871c16820a32f00b

---
+++ sample/test/hello.go
@@ -0,0 +1,14 @@
+// Copyright 2016 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello, there, it is", time.Now())
+}
